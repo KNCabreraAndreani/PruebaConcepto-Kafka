488979204f48f60fe69484a6ebf76fb104d9ade6
Cabrera Kevin <kcabrera@andreani.com>
2021-08-10 13:23:11 -0300

    Agregar archivos de proyecto.

---
+++ Api/Services/KafkaConsumerService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+using Kafka.Public;
+using Kafka.Public.Loggers;
+using System.Text;
+
+namespace Api.Services
+{
+    public class KafkaConsumerService : IHostedService
+    {
+        private readonly ILogger<KafkaConsumerService> _logger;
+        private readonly ClusterClient _cluster;
+        public KafkaConsumerService(ILogger<KafkaConsumerService> logger)
+        {
+
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration
+            {
+                Seeds = "localhost:9092"
+            }, new ConsoleLogger());
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cluster.ConsumeFromLatest("demo");
+            _cluster.MessageReceived += record =>
+            {
+                _logger.LogInformation($"Received: {Encoding.UTF8.GetString(record.Value as byte[])}");
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cluster?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Api/Services/KafkaProducerService.cs
@@ -0,0 +1,46 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Api.Services
+{
+    public class KafkaProducerService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerService> _logger;
+        private IProducer<Null, string> _producer;
+        public KafkaProducerService(ILogger<KafkaProducerService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var value = $"Hello World {i}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("demo", new Message<Null, string>()
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+
+            _producer.Flush(TimeSpan.FromSeconds(10));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
